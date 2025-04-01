ee7b0436627a96441d3cbd3fdbf053132fabb595
yogitha <139643769+yogitha08@users.noreply.github.com>
2025-04-01 14:13:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var type = new Typed(".text", {
+        strings: ["Frontend Developer", "Full Stack Developer","Java programmer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
